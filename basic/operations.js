91fc966254b6b45b39e55852612ff1e6209d8ea8
Jadhavpree <163329145+Jadhavpree@users.noreply.github.com>
2025-12-11 11:35:18 +0000

    operations in js

---
+++ basic/operations.js
@@ -0,0 +1,16 @@
+let value =3;
+let neVal=-value;
+//console.log(neVal);
+
+let str1="hello ";
+let str2="prstik";
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1"+2);//12
+console.log("1"+"2");//12
+console.log(1+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"3");//33 acts as which one is come first
+console.log(+ture);//1
+console.log(+"")//0
