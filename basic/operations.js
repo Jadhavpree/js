9e05de53f10b117c06dddcb4bb7f0611f254bef5
Jadhavpree <163329145+Jadhavpree@users.noreply.github.com>
2025-12-11 11:50:42 +0000

    changes

--- basic/operations.js
+++ basic/operations.js
@@ -11,6 +11,15 @@ console.log("1"+2);//12
 console.log("1"+"2");//12
 console.log(1+"2");//12
 console.log("1"+2+2);//122
+
 console.log(1+2+"3");//33 acts as which one is come first
-console.log(+ture);//1
+console.log(+true);//1
 console.log(+"")//0
+console.log("2">1);//true automatically convert it to number
+console.log(null>0);
+console.log(null<0);
+console.log(null==0);//false
+console.log(null>=0);//true
+//undefined false after compare with 0
+// == check with value only without dt
+//=== with val and dt 
