151da14aef0f81beb916246d9b677d8ce5a70591
Jadhavpree <163329145+Jadhavpree@users.noreply.github.com>
2025-12-11 10:54:22 +0000

    dattypes

---
+++ basic/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//code treated as newer version of js
+//alert("3+3");//not executed here as we use node 
+             //for running it we need it to run at browser
+
+//datatypes:
+//number=2 to power 53
+//BigInt
+//string
+//boolean
+//null:standalone value ,rep of empty value
+//undefined:when value is not defined
+//symbol:for the uniqueness 
+
+
+//non primative 
+//object
+
+console.log(typeof(num));//here nuber is not defined so type is undefined
+console.log(typeof(null));//shows the type object 
+console.log(typeof(undefined));//show type undefined
