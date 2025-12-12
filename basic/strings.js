e7628504316e11b6949d56dda34d57292d1c5496
Jadhavpree <163329145+Jadhavpree@users.noreply.github.com>
2025-12-12 12:00:15 +0000

    string manupulation

---
+++ basic/strings.js
@@ -0,0 +1,6 @@
+const name="pratiksha";
+const lastname="Jadhav";
+//use a backtiks that string interpolation creates a placeholders and directly injects the var
+console.log(`name is:${name} and last name is:${lastname}`);
+
+const gameName =new String('ludo');
